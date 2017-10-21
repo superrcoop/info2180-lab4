61dccf19a0cf7b2ad429eb8ad5dc4467ddf4664b
Jordan Cooper <superrcoop@Jordans-MacBook-Pro.local>
2017-10-20 22:28:04 -0500

    borders turn red when hit

--- maze.js
+++ maze.js
@@ -1,17 +1,28 @@
+var loser=false;
+
 window.onload = function(){
-    //Exercise 1
-    var a = document.getElementById("boundary1");
-    a.onmouseover = function()
-        {
-            this.setAttribute("class","boundary youlose");
-            this.style.backgroundColour = "#ff8888";
-
-        }
-
-    //Excercise 2
     var boundaries = document.querySelectorAll(".boundary"); 
-    var check = false;
-            
-    
-    
+    for(var i=0; i < boundaries.length; i++){
+        boundaries[i].onmouseover = gameOver;
+    }      
+};
+
+function gameOver(){
+    loser=true;
+    if(!loser){
+        document.getElementById('status').innerHTML="You Win! good job";
+    }
+    else{
+        document.getElementById('status').innerHTML="Loser";
+        this.setAttribute("class", "boundary youlose");
+
+    }
+
 }
+
+function start(){
+    var start = document.getElementById("start").onclick= function (){
+
+    }
+}
+
