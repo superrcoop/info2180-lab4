2335fcdfe1cdc2995b75900693e4cb1b7d32d07f
Jordan Cooper <superrcoop@Jordans-MacBook-Pro.local>
2017-10-20 23:10:06 -0500

    winner winner chicken dinner

--- maze.js
+++ maze.js
@@ -1,28 +1,38 @@
 var loser=false;
 
+
 window.onload = function(){
     var boundaries = document.querySelectorAll(".boundary"); 
     for(var i=0; i < boundaries.length; i++){
-        boundaries[i].onmouseover = gameOver;
-    }      
+        boundaries[i].addEventListener("mouseover",gameOver);
+    }    
+
+    document.getElementById("start").addEventListener("click", start);  
+    document.getElementById("end").addEventListener("mouseover", endGame);
 };
 
 function gameOver(){
     loser=true;
-    if(!loser){
-        document.getElementById('status').innerHTML="You Win! good job";
+    document.getElementById('status').innerHTML="Loser";
+    var boundaries = document.querySelectorAll(".boundary"); 
+    for (var i = 0; i < boundaries.length-1; i++) {
+        boundaries[i].className += " youlose";
     }
-    else{
-        document.getElementById('status').innerHTML="Loser";
-        this.setAttribute("class", "boundary youlose");
-
-    }
-
 }
 
 function start(){
-    var start = document.getElementById("start").onclick= function (){
+    var boundary = document.querySelectorAll(".boundary");
+    for (var i = 0; i < boundary.length-1; i++) {
+        boundary[i].className = "boundary";
+        document.getElementById("status").innerHTML="Retry";
+    } 
 
-    }
 }
 
+function endGame(){
+    if(!loser){
+       document.getElementById("status").innerHTML="You Win!";
+       loser = false;
+    }
+
+}
